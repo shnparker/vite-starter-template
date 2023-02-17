1e2a110ebefbe9ec5cdfaffdcccc24c4a414a7f7
Shane Parker <sparker2@godaddy.com>
2023-02-17 21:31:13 +0200

    remove testing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import test from '@/utils/test'
 function App() {
-  return <h1 className="text-green-500">Hello World - {test()}</h1>
+  return <h1 className="text-green-500">Hello World</h1>
 }
 
 export default App
