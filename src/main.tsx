775126e7241fc3362a32f5797a6912f1001194a2
Shane Parker <sparker2@godaddy.com>
2023-03-07 16:47:54 +0200

    fix job requirements

--- src/main.tsx
+++ src/main.tsx
@@ -1,45 +1,14 @@
-import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ErrorBoundary } from 'react-error-boundary'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import './index.css'
 
-const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error, query) => {
-      if (query.state.data !== undefined) {
-        // some toast library
-      }
-
-      console.log(error)
-    },
-  }),
-  defaultOptions: {
-    queries: {
-      retry: 0,
-    },
-    mutations: {
-      retry: 0,
-    },
-  },
-})
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary
-      FallbackComponent={() => <div>Oops! fallback</div>}
-      onError={() => console.log('Report to some error platform')}
-    >
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-      </QueryClientProvider>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
